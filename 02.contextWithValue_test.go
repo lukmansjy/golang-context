db445902992e27d46fdba3d4aa43950053eaba98
Lukman Sanjaya <lukman.rocks@live.com>
2022-06-12 09:51:48 +0700

    first commit

---
+++ 02.contextWithValue_test.go
@@ -0,0 +1,33 @@
+package golang_context
+
+import (
+	"context"
+	"fmt"
+	"testing"
+)
+
+func TestContextWithValue(t *testing.T) {
+	contextA := context.Background()
+
+	contextB := context.WithValue(contextA, "b", "B")
+	contextC := context.WithValue(contextA, "c", "C")
+
+	contextD := context.WithValue(contextB, "d", "D")
+	contextE := context.WithValue(contextB, "e", "E")
+
+	contextF := context.WithValue(contextC, "f", "F")
+	contextG := context.WithValue(contextE, "g", "G")
+
+	fmt.Println(contextA)
+	fmt.Println(contextB)
+	fmt.Println(contextC)
+	fmt.Println(contextD)
+	fmt.Println(contextE)
+	fmt.Println(contextF)
+	fmt.Println(contextG)
+
+	fmt.Println(contextF.Value("f")) // dapat
+	fmt.Println(contextF.Value("c")) // dapat milik parent
+	fmt.Println(contextF.Value("b")) // tidak dapat, beda parent
+	fmt.Println(contextA.Value("b")) // tidak bisa mangambil data child
+}
